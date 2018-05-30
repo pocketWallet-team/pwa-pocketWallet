7a4ba8cfba5f7ea5cf526dbde1e748fbf7c05a48
Seifallah GUEDOUARI <guedouari@gmail.com>
2018-05-30 14:11:32 +0000

    prepare release

--- scripts/patch-gh-pages.sh
+++ scripts/patch-gh-pages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#abort() {
-#    local message=$1
-#    echo $message
-#    exit -1
-#}
-#[ -z $TRAVIS_BRANCH ] && abort "branch is undefined"
-#if [ "$TRAVIS_BRANCH" = "next" ]
-#then
+abort() {
+    local message=$1
+    echo $message
+    exit -1
+}
+[ -z $TRAVIS_BRANCH ] && abort "branch is undefined"
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
 	cp "$TRAVIS_BUILD_DIR/dist/pwa-pocketWallet/index.html" "$TRAVIS_BUILD_DIR/dist/pwa-pocketWallet/404.html"
 	echo "created 404.html for gh-pages"
-#else
+else
 	echo "nothing to do ..."
-#fi
+fi
